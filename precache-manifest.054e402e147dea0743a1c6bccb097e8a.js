edaa5fd9de3263875c7562ef44eb7a2df121b8bb
victorlian <vlia679@aucklanduni.ac.nz>
2018-11-12 01:56:36 +1300

    Updates

---
+++ precache-manifest.054e402e147dea0743a1c6bccb097e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/five-in-a-row-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76d517f179aecdd9435f",
+    "url": "/five-in-a-row-gh-pages/static/js/runtime~main.76d517f1.js"
+  },
+  {
+    "revision": "2f22bcf4888c8774f52c",
+    "url": "/five-in-a-row-gh-pages/static/js/main.2f22bcf4.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/five-in-a-row-gh-pages/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "2f22bcf4888c8774f52c",
+    "url": "/five-in-a-row-gh-pages/static/css/main.bfc50010.chunk.css"
+  },
+  {
+    "revision": "887800527d1d82fd6bff9f39ef142eaa",
+    "url": "/five-in-a-row-gh-pages/index.html"
+  }
+];
